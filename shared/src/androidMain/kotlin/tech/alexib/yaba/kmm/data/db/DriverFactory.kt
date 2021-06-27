1166bf12ed24c08254346c510b35735926d121f7
ruffCode <alexi@ruffcode.com>
2021-06-26 22:21:46 -0400

    [fix] (Android) Context instead of Application in DriverFactory

--- shared/src/androidMain/kotlin/tech/alexib/yaba/kmm/data/db/DriverFactory.kt
+++ shared/src/androidMain/kotlin/tech/alexib/yaba/kmm/data/db/DriverFactory.kt
@@ -1,11 +1,12 @@
 package tech.alexib.yaba.kmm.data.db
 
 import android.app.Application
+import android.content.Context
 import com.squareup.sqldelight.android.AndroidSqliteDriver
 import com.squareup.sqldelight.db.SqlDriver
 import tech.alexib.yaba.data.db.YabaDb
 
-actual class DriverFactory(private val context: Application){
+actual class DriverFactory(private val context: Context){
     actual fun createDriver():SqlDriver {
         return AndroidSqliteDriver(YabaDb.Schema, context, "yaba.db")
     }
