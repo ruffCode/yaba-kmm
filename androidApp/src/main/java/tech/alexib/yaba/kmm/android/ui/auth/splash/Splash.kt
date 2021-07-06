05a1da1294992bc5eac93fd1d0ae38acb8484a03
Alexi Bre <alexi@ruffcode.com>
2021-07-06 16:11:30 -0400

    cleanup resources

--- androidApp/src/main/java/tech/alexib/yaba/kmm/android/ui/auth/splash/Splash.kt
+++ androidApp/src/main/java/tech/alexib/yaba/kmm/android/ui/auth/splash/Splash.kt
@@ -1,20 +1,14 @@
 package tech.alexib.yaba.kmm.android.ui.auth.splash
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
-import com.google.accompanist.coil.rememberCoilPainter
-import tech.alexib.yaba.kmm.android.R
+import tech.alexib.yaba.kmm.android.ui.components.YabaLogo
 import tech.alexib.yaba.kmm.android.ui.theme.BlueSlate
 
 @Composable
@@ -29,17 +23,7 @@ fun Splash(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Image(
-            modifier = Modifier
-                .padding(16.dp)
-                .height(200.dp),
-            painter = rememberCoilPainter(
-                R.drawable.yaba_nbg,
-                previewPlaceholder = R.drawable.yaba_nbg
-            ),
-            contentScale = ContentScale.Fit,
-            contentDescription = "$",
-        )
+        YabaLogo()
     }
 }
 
