876b710d59b843174eca713c15ab8736baa01a9b
ruffCode <alexi@ruffcode.com>
2021-09-12 00:14:30 -0400

    cleanup PlaidLinkResultScreenViewModel

--- androidApp/src/main/kotlin/tech/alexib/yaba/android/ui/plaid/PlaidLinkResultScreenViewModel.kt
+++ androidApp/src/main/kotlin/tech/alexib/yaba/android/ui/plaid/PlaidLinkResultScreenViewModel.kt
@@ -17,8 +17,6 @@ package tech.alexib.yaba.android.ui.plaid
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import co.touchlab.kermit.Kermit
-import kotlinx.coroutines.flow.MutableStateFlow
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.koin.core.parameter.parametersOf
@@ -31,63 +29,10 @@ class PlaidLinkResultScreenViewModel : ViewModel(), KoinComponent {
 
     val store: PlaidLinkResultStore by inject { parametersOf(viewModelScope) }
 
-    //    private val loader = ObservableLoadingCounter()
-//    private val setAccountsToHide: SetAccountsToHide by inject()
-//    private val addItem: AddItem by inject()
-
-    private val log: Kermit by inject { parametersOf("PlaidLinkResultScreenViewModel") }
-    private val accountsFlow = MutableStateFlow<List<PlaidLinkScreenResult.Account>>(emptyList())
-
-
-    //    private val loadingFlow = MutableStateFlow(false)
-//    val loading: StateFlow<Boolean>
-//        get() = loadingFlow
-    private val shouldNavigateHomeFlow = MutableStateFlow(false)
-//    val shouldNavigateHome: StateFlow<Boolean>
-//        get() = shouldNavigateHomeFlow
-//    val accounts: StateFlow<List<PlaidLinkScreenResult.Account>>
-//        get() = accountsFlow
-
     fun init(plaidLinkScreenResult: PlaidLinkScreenResult) {
         store.init(plaidLinkScreenResult)
     }
 
     val state = store.state.stateInDefault(viewModelScope, PlaidLinkResultScreenState.Empty)
 
-
-//    fun setAccountShown(plaidAccountId: String, show: Boolean) {
-//        val currentAccounts = accountsFlow.value.toMutableList()
-//        val currentItem = currentAccounts.first { it.plaidAccountId == plaidAccountId }
-//        currentAccounts[currentAccounts.indexOf(currentItem)] = currentItem.copy(show = show)
-//        accountsFlow.value = currentAccounts
-//    }
-//
-//    val state = combine(
-//        loader.observable,
-//        accountsFlow,
-//        shouldNavigateHomeFlow
-//    ) { loading, accounts, shouldNavigate ->
-//        PlaidLinkResultScreenState(loading, shouldNavigate, accounts)
-//    }.stateInDefault(viewModelScope, PlaidLinkResultScreenState.Empty)
-//
-//    fun submitAccountsToHide() {
-//        val accountsToHide = accountsFlow.value.filter { !it.show }.map { it.plaidAccountId }
-//        viewModelScope.launch(Dispatchers.Default) {
-//
-//            setAccountsToHide(SetAccountsToHide.Params(itemId, accountsToHide)).collect {
-//                when (it) {
-//                    is InvokeStarted -> loader.addLoader()
-//                    is InvokeSuccess -> addItem(AddItem.Params(itemId)).collect { status ->
-//                        if (status != InvokeStarted) {
-//                            loader.removeLoader()
-//                            shouldNavigateHomeFlow.emit(true)
-//                        }
-//                    }
-//                    is InvokeError -> {
-//                        log.e(it.throwable) { "Error loading transactions" }
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
